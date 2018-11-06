2baeca0bc9c255f14bc3f021f940cf48f77b52c2
LGMchili <520100297@qq.com>
2018-11-06 09:41:47 +0800

    fix cirGen

--- python/cirGen.py
+++ python/cirGen.py
@@ -1,5 +1,5 @@
 
-n = 1000
+n = 100
 total_num = 2 * n * (n - 1)
 with open('tstCir.sp', 'w') as file:
     # horizontal
@@ -25,12 +25,12 @@ with open('tstCir.sp', 'w') as file:
             y += 1
             file.write(comp + '\n')
     # add voltage pad
-    csrc = 'i0 ' + 'x_0 gnd ' + '-1'
+    csrc = 'i0 ' + 'x_0 gnd ' + '-0.8'
     rpad = 'r_pad ' + 'x_0 gnd ' + '1.25'
-    lpad = 'l_pad ' + 'x_0 n1 ' + '1e-5'
-    cpad = 'c_pad ' + 'x_0 gnd ' + '1e-5'
+    lpad = 'l_pad ' + 'x_0 n1 ' + '1e-6'
+    cpad = 'c_pad ' + 'n' + str(y + n - 1) + ' gnd ' + '1e-5'
     load = 'i_load ' + 'n' + str(y + n - 1) + ' gnd ' + ' pwl ' \
             + '.35e-3 0 .4e-3 0.5 .45e-3 .3 .5e-3 .8 .58e-3 0'
-    directive = '.tran 0 2e-3 0.01e-3'
+    directive = '.tran 0 2e-3 0.001e-3'
     for comp in [csrc, rpad, lpad, cpad, load, directive]:
         file.write(comp + '\n')
